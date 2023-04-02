39a56eb8e832e7dea28352ce80e8d971bfb8accc
Mario <i.am@mariosanchez.pw>
2023-04-02 15:40:37 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://supermariochat.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
